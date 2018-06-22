741d76ef6db761eed770fd772556d1f5f73f2a83
devgabrielugalde <gabriel.10.delima@gmail.com>
2018-06-21 23:42:41 -0400

    frases

--- Frases.js
+++ Frases.js
@@ -12,31 +12,84 @@ function getFraseDia (dia) {
 	    case '25':
 	        return "Iluminados pela ressurreição de Cristo, peregrinamos (física ou espiritualmente) até o Santuário de Fátima, Deus sabe o caminho que cada um teve que percorrer para chegar até aqui, mas como é consolador estar diante da imagem de Nossa Senhora! Seu olhar nos transmite uma doçura imensa, sua ternura nos envolve como num abraço de mãe. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-
 		case '24':
 	        return "Aquele abraço com que a mãe recebe o filho que chega, e não dá para definir de quem é a maior alegria do filho ou da mãe. E assim em seu colo, sem precisar de palavras, todas as nossas dores, lágrimas, nossas misérias, pecados, se transformam. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
 		case '23':
-	        ""
+	        return "Seu olhar atravessa o mais íntimo de nossa alma, e nossa vida inteira nos parece ser compreendida. Sentimos-nos amados, sem reservas, infinitamente. Então recostados em seu colo ousamos dizer: “Mãe, cheguei!” E ela com toda sua ternura nos diz: “Filhinhos!” E o som de sua voz fica gravado em nosso coração, como sinal de ressurreição, vida nova que começa. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-		case '':
-	        ""
+		case '22':
+	        return "O nosso coração se expande diante da graça e aos poucos o mistério vai se revelando, então descobrimos que nosso coração bate ritmado pelo mistério da vida de Cristo e de sua ascensão ao céu, ritmado pela presença maternal de Maria, que sempre esteve ao nosso lado. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-		case '':
-	        ""
+		case '21':
+	        return "E até os recantos mais escuros de nossa alma parecem agora descobertos, em paz. Uma pessoa que se sente assim tão amada sente também o desejo de amar, de devolver ao mundo esta graça. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-		case '':
-	        ""
+		case '20':
+	        return "Sentemos ao lado da mãe e façamos da oração que o anjo ensinou aos pastorinhos, a nossa oração para pedir a graça de Deus pelo mundo. No ritmo tranquilo da nossa respiração rezemos: Eu creio, adoro, espero e vos amo. Peço-vos pelos que não creem, não adoram, não esperam e não vos amam. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-		case '':
-	        ""
+		case '19':
+	        return "O Espírito Santo derramou-se sobre os apóstolos e a Mãe estava com eles. A presença da Virgem Maria no nascimento da Igreja é prelúdio de seu auxílio na vida de todos os que se deixam tocar pela certeza da sua presença. Mistério que palavras não podem expressar, mas que é sentido pela nossa alma. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
 	        break;
-		case '':
-	        ""
+		case '18':
+	        return "Em Fátima a Virgem Maria pede aos pastorinhos para rezar, rezar muito e fazer sacrifício pelos pecadores, pois muitas almas se perdem por não haver quem se sacrifique e peça por elas. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+        case '17':
+	        return "Mãe, nós te pedimos e a ti confiamos os nossos lares, felizes e os que vivem em conflito. Confiamos aos teus cuidados os nossos jovens, pois quem segura na tua mão não erra o caminho. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '16':
+	        return "Confiamos aos teus cuidados, os que estão aflitos, pelas suas dores ou daqueles que Amam. Pedimos-te pela conversão de todos nós pecadores. Que o teu olhar de mãe ilumine os caminhos da nossa vida, aumentando em nós a fé, esperança e caridade. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '15':
+	        return "Porque olhou para a humildade de sua serva. De agora em diante, todas as gerações me chamarão bem-aventurada, pois o poderoso fez grandes coisas em meu favor, santo é o seu nome (Lc 1, 48-49). Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '14':
+	        return "O meu coração Imaculado será o teu refúgio e o caminho que te conduzirá até Deus”, prometeu Maria a Lúcia em Fátima, em 1917. Esta promessa não é só para a Lúcia. É para todos nós, que hoje oramos no Santuário de Fátima ou nos muitos lugares onde Deus nos espera. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '13':
+	        return "Nos acontecimentos de Fátima vimos o amor, a profunda sensibilidade de Deus face aos homens e mulheres. Os corações de Jesus e de Maria estão atentos à voz das nossas súplicas. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+        case '12':
+	        return "Supliquemos também nós pelo Movimento das Equipes de Nossa Senhora, que cada casal, cada sacerdote, cada equipe tenham como desejo primeiro caminhar para a santidade, nem mais nem menos. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '11':
+	        return "Que Nossa Senhora, que hoje invocamos sob o título de Nossa Senhora de Fátima, rogue a Deus por cada um de nós. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '10':
+	        return "Apareceu no céu um grande sinal: uma mulher vestida de sol, com a lua debaixo dos seus pés e uma coroa de doze estrelas sobre a cabeça (Ap 12. 1). Deus tem um desejo em relação a nós. Ele conhece a nossa história, as nossas características, as nossas capacidades e limites muito mais profundamente do que nós mesmos. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '09':
+	        return "Ele confia e espera que nossa vida seja um reflexo do seu amor pela humanidade. Jacinta, Lúcia e Francisco, embora pequeninos, se fizeram luz para o mundo, acolhendo e anunciando a mensagem daquela que foi escolhida por Deus para ser a Rainha do céu e da terra, porque mãe do nosso Rei Salvador, Jesus Cristo. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '08':
+	        return "E Ele continua passando nas ruas e cidades e lançando o seu convite. Pe. Caffarel ouviu a sua voz: “Oh, nada de extraordinário naquele longínquo dia de março eu soube que era amado e que amava”, isto permeou todo o sentido de sua existência. Homem de grande sabedoria e profundo conhecimento, mostrava-se humilde e simples ao convidar os irmãos a segui-lo no caminho de Jesus Cristo. Agora no céu, junto à Virgem Maria, olha por nós. Os filhos que gerou pela fé. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+        case '07':
+	        return "E nós aos pés de Nossa Senhora, no Santuário de Fátima, lugar de bênção e graças, vimos pedir a Deus: que apresse o dia em que a igreja há de proclamar a santidade de sua vida. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '06':
+	        return "Oração: Santíssima Virgem, que nos montes de Fátima vos dignastes revelar aos três pastorinhos os tesouros das graças que podemos alcançar, rezando o Santo Rosário, ajudai-nos a apreciar sempre mais essa santa oração, a fim de que, meditando os mistérios da nossa redenção, alcancemos as graças que insistentemente vos Pedimos. Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '05':
+	        return "Turistas em busca de sensações? Não. Mas peregrinos em busca de Deus.” Pe. Caffarel , CM 1959-9 Fonte: O ROSÁRIO - CM 517, Páginas 19 a 21."
+	        break;
+		case '04':
+	        return "É o Movimento todo que se movimentou, uns poucos receberam a incumbência de serem os delegados dos demais... O auxílio mútuo permitiu a viagem de alguns... E estes ao partirem levaram consigo as preces, as súplicas, os anseios e as ações de graças daqueles que ficaram. Extraído do Artigo de Vera e José Renato Luciani, Casal Coordenador Nacional EI FÁTIMA 2018 - CM 517, Página 23."
+	        break;
+		case '03':
+	        return "... da primeira peregrinação das ENS de caráter internacional, em Roma: “A Peregrinação é do Movimento...somos todos nós que iremos participar deste grande elã comum e das graças que o Senhor reserva àqueles que o procuram desse modo” ...trechos da CM 1959-5 Extraído do Artigo de Vera e José Renato Luciani, Casal Coordenador Nacional EI FÁTIMA 2018 - CM 517, Página 23."
+	        break;
+        case '02':
+	        return "Compreendemos que por ser uma iniciativa coletiva, todos os que ficam são peregrinos como aqueles que foram. Somos todos chamados a assumir o espírito peregrino: romper a rotina, caminhar com outro, acolher suas alegrias e suas dores e oferecer a Deus e comunicar com alegria o Senhor Ressuscitado. Extraído do Artigo de Vera e José Renato Luciani, Casal Coordenador Nacional EI FÁTIMA 2018 - CM 517, Página 23."
+	        break;
+		case '01':
+	        return "Que a motivação maior para atravessar o Atlântico seja o espírito de peregrinação. Que sejamos dignos representantes dos primeiros casais peregrinos brasileiros em Roma. Que a nossa participação seja efetiva e frutuosa, porque hoje, como ontem, grande parte dos casais, sem exclusão dos equipistas, tem necessidade de serem ajudados com as graças do matrimônio cristão. Que caminhemos como Maria, a Senhora de Fátima, peregrina na fé. Extraído do Artigo de Vera e José Renato Luciani, Casal Coordenador Nacional EI FÁTIMA 2018 - CM 517, Página 23."
+	        break;
+		case '00':
+	        return "Em algumas horas irá começar!"
 	        break;
 
 	    default:
-	        "";
+	        return "";
 	        break
 	}
 
